047e13889d26cd7775102759df90890aa71deecb
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-06-17 17:01:10 +0330

    add: search feature

--- pages/productsPage.js
+++ pages/productsPage.js
@@ -1,24 +1,75 @@
 import axios from "axios";
 import { PRODUCT_URL } from "../api/api";
 
-
 const container = document.getElementById('app');
 
+
 export function productsPage() {
-  container.innerHTML = '<div class="flex items-center flex-col my-6 w-full"><h1 class="text-2xl font-bold text-center text-gray-800 mb-8" >Our Products</h1><div id="products-container" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8"></div></div>';
-  displayProducts();
+  container.innerHTML = "";
+  container.className = 'flex items-center flex-col my-6 w-full';
+
+  const heading = document.createElement('h1');
+  heading.className = 'text-2xl font-bold text-center text-gray-800 mb-8';
+  heading.textContent = 'Our Products';
+  container.appendChild(heading);
+
+  const searchContainer = document.createElement('div');
+  searchContainer.className = 'flex mx-auto w-fit relative';
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.name = 'name';
+  input.id = 'search-input';
+  input.placeholder = 'Enter a name...';
+  input.required = true;
+  input.className = 'w-80 border border-gray-300 rounded-lg h-10 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500';
+  searchContainer.appendChild(input);
+
+  const searchBtn = document.createElement('img');
+  searchBtn.src = './assets/search.png';
+  searchBtn.alt = '_';
+  searchBtn.id = 'search-btn';
+  searchBtn.className = 'w-5 h-5 absolute right-4 top-3 cursor-pointer';
+  searchContainer.appendChild(searchBtn);
+
+  container.appendChild(searchContainer);
+
+  const productsContainer = document.createElement('div');
+  productsContainer.id = 'products-container';
+  productsContainer.className = 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12';
+  container.appendChild(productsContainer);
+  fetchProducts(input.value);
 }
 
 
-async function fetchProducts() {
+async function fetchProducts(searchKey) {
+  let response = "";
+  let result = "";
   try {
-    const response = await fetch(PRODUCT_URL);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    return [];
+    if (searchKey !== "") {
+      response = await fetch(`${PRODUCT_URL}?name=${searchKey}`);
+    } else {
+      response = await fetch(PRODUCT_URL)
+    }
+
+    if (response.ok) {
+      result = await response.json();
+      result = result.sort((a, b) => {
+        return a.id - b.id;
+      })
+    } else if (!response.ok) {
+      console.log(response.statusText)
+    }
+
+  } catch (e) {
+    console.log(e)
   }
+  const productsContainer = document.getElementById('products-container');
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  result.forEach(product => {
+    const productCard = createProductCard(product);
+    productsContainer.appendChild(productCard);
+  });
 }
 
 function createProductCard(product) {
@@ -46,16 +97,3 @@ function createProductCard(product) {
   return card;
 }
 
-async function displayProducts() {
-  const productsContainer = document.getElementById('products-container');
-  console.log(productsContainer);
-
-
-  const products = await fetchProducts();
-
-  // biome-ignore lint/complexity/noForEach: <explanation>
-  products.forEach(product => {
-    const productCard = createProductCard(product);
-    productsContainer.appendChild(productCard);
-  });
-}
